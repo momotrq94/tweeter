9509a148f6f491a75371ccf3a71d7cfe188f29da
Muhammed Tariq <momotrq94@gmail.com>
2022-04-26 17:03:05 -0400

    add color change to character counter to turn red when -ve characters

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
     let message = $(this).val();
     let output = 140 - message.length;
     let outputCounter = $(this).siblings().children(".counter");
+    if (output < 0) {
+      outputCounter.css("color", "red");
+    }
+    if (output >= 0) {
+      outputCounter.css("color", "DimGray");
+    }
     $(outputCounter).val(output);
   });
 });
